8c5ad9395d6f4ab0efa1107f63bdfcb83b60e326
Dushyant Singh <dushyantsinghjubbal@gmail.com>
2025-10-31 12:00:56 +0000

    This the error in browser Automatic initialization failed. Falling back

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -6,15 +6,11 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore'
 
 let firebaseApp: FirebaseApp;
+
+// If no Firebase app has been initialized, initialize one.
+// This is the standard way to initialize for client-side Next.js apps.
 if (!getApps().length) {
-    try {
-      firebaseApp = initializeApp();
-    } catch (e) {
-      if (process.env.NODE_ENV === "production") {
-        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
-      }
-      firebaseApp = initializeApp(firebaseConfig);
-    }
+    firebaseApp = initializeApp(firebaseConfig);
 } else {
     firebaseApp = getApp();
 }
